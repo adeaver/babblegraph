6de6b2ac0c7c97af752156cace00f95d56b90a75
Andrew Deaver <deaver.andrew@gmail.com>
2020-09-19 17:02:19 -0400

    Add decimal

--- lib/math/decimal/decimal.go
+++ lib/math/decimal/decimal.go
@@ -2,62 +2,62 @@ package decimal
 
 const percision = 1000000
 
-type DecimalNumber struct {
+type Number struct {
 	millionths int64
 }
 
-func FromInt64(i int64) DecimalNumber {
-	return DecimalNumber{
+func FromInt64(i int64) Number {
+	return Number{
 		millionths: i * percision,
 	}
 }
 
-func FromFloat64(f float64) DecimalNumber {
-	return DecimalNumber{
+func FromFloat64(f float64) Number {
+	return Number{
 		millionths: int64(float64(percision) * f),
 	}
 }
 
-func (d DecimalNumber) Multiply(n DecimalNumber) DecimalNumber {
-	return DecimalNumber{
+func (d Number) Multiply(n Number) Number {
+	return Number{
 		millionths: (d.millionths * n.millionths) / percision,
 	}
 }
 
-func (d DecimalNumber) Divide(n DecimalNumber) DecimalNumber {
-	return DecimalNumber{
+func (d Number) Divide(n Number) Number {
+	return Number{
 		millionths: int64((float64(d.millionths) / float64(n.millionths)) * float64(percision)),
 	}
 }
 
-func (d DecimalNumber) Add(n DecimalNumber) DecimalNumber {
-	return DecimalNumber{
+func (d Number) Add(n Number) Number {
+	return Number{
 		millionths: (d.millionths + n.millionths),
 	}
 }
 
-func (d DecimalNumber) Subtract(n DecimalNumber) DecimalNumber {
-	return DecimalNumber{
+func (d Number) Subtract(n Number) Number {
+	return Number{
 		millionths: (d.millionths - n.millionths),
 	}
 }
 
-func (d DecimalNumber) GreaterThan(n DecimalNumber) bool {
+func (d Number) GreaterThan(n Number) bool {
 	return d.millionths > n.millionths
 }
 
-func (d DecimalNumber) GreaterThanOrEqualTo(n DecimalNumber) bool {
+func (d Number) GreaterThanOrEqualTo(n Number) bool {
 	return d.millionths >= n.millionths
 }
 
-func (d DecimalNumber) LessThan(n DecimalNumber) bool {
+func (d Number) LessThan(n Number) bool {
 	return d.millionths < n.millionths
 }
 
-func (d DecimalNumber) LessThanOrEqualTo(n DecimalNumber) bool {
+func (d Number) LessThanOrEqualTo(n Number) bool {
 	return d.millionths <= n.millionths
 }
 
-func (d DecimalNumber) EqualTo(n DecimalNumber) bool {
+func (d Number) EqualTo(n Number) bool {
 	return d.millionths == n.millionths
 }
