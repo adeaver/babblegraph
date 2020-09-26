a109aa0f4c8cc5705a1901dca3e55b86035e6a67
Andrew Deaver <deaver.andrew@gmail.com>
2020-09-26 14:59:49 -0400

    Fix index

--- lib/model/index/query.go
+++ lib/model/index/query.go
@@ -5,7 +5,7 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-const insertTermEntry = "INSERT INTO document_term_entries (document_id, term, count) VALUES ($1, $2, $3)"
+const insertTermEntry = "INSERT INTO document_term_entries (document_id, term_id, count) VALUES ($1, $2, $3)"
 
 func InsertTermEntry(tx *sqlx.Tx, documentID documents.DocumentID, term string, count int64) error {
 	_, err := tx.Exec(insertTermEntry, documentID, term, count)
