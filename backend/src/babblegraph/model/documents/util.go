4a1da8394919b4a0ff559a8ee708905ced4ac33c
Andrew Deaver <deaver.andrew@gmail.com>
2020-11-11 17:37:23 -0500

    Whoops

--- backend/src/babblegraph/model/documents/util.go
+++ backend/src/babblegraph/model/documents/util.go
@@ -4,9 +4,10 @@ import (
 	"babblegraph/util/urlparser"
 	"crypto/md5"
 	"encoding/hex"
+	"fmt"
 )
 
 func makeDocumentIndexForURL(parsedURL urlparser.ParsedURL) DocumentID {
 	md5Hash := md5.Sum([]byte(parsedURL.URLIdentifier))
-	return DocumentID(hex.EncodeToString(md5Hash[:]))
+	return DocumentID(fmt.Sprintf("web_doc-%s", hex.EncodeToString(md5Hash[:])))
 }
