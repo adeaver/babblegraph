9546d77c56abdc8224d264b4d557ee61a40fa3ba
Andrew Deaver <deaver.andrew@gmail.com>
2020-11-24 16:02:39 -0600

    Add description to email template

--- backend/src/babblegraph/services/email/dailyemail/send_email.go
+++ backend/src/babblegraph/services/email/dailyemail/send_email.go
@@ -12,7 +12,7 @@ import (
 func sendDailyEmailsForDocuments(cl *email.Client, recipient string, docs []documents.Document) error {
 	var links []email.DailyEmailLink
 	for _, doc := range docs {
-		var title, imageURL *string
+		var title, imageURL, description *string
 		if doc.Metadata != nil {
 			if len(doc.Metadata.Title) > 0 {
 				title = ptr.String(doc.Metadata.Title)
@@ -20,15 +20,19 @@ func sendDailyEmailsForDocuments(cl *email.Client, recipient string, docs []docu
 			if len(doc.Metadata.Image) > 0 && urlparser.IsValidURL(doc.Metadata.Image) {
 				imageURL = ptr.String(doc.Metadata.Image)
 			}
+			if len(doc.Metadata.Description) > 0 {
+				description = ptr.String(doc.Metadata.Description)
+			}
 		}
 		if !urlparser.IsValidURL(doc.URL) {
 			continue
 		}
 		log.Println(fmt.Sprintf("Sending URL %s to recipient %s", doc.URL, recipient))
 		links = append(links, email.DailyEmailLink{
-			ImageURL: imageURL,
-			Title:    title,
-			URL:      doc.URL,
+			ImageURL:    imageURL,
+			Title:       title,
+			Description: description,
+			URL:         doc.URL,
 		})
 	}
 	return cl.SendDailyEmailForLinks(recipient, links)
