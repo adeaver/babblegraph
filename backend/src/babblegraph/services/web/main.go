cf8b51be99dc5e93d66128bf4a158e4b4d2acbfe
Andrew Deaver <deaver.andrew@gmail.com>
2020-12-03 15:24:38 -0600

    Use React Frontend (#52)

--- backend/src/babblegraph/services/web/main.go
+++ backend/src/babblegraph/services/web/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"babblegraph/util/env"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	log.Println("Starting babblegraph web server")
 	r := mux.NewRouter()
+	staticFileDirName := env.MustEnvironmentVariable("STATIC_DIR")
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1>Babblegraph.com is under construction\n</h1>")
+	// TODO: put API router in
+	// apiRouter := r.PathPrefix("/api")
+	r.PathPrefix("/dist").Handler(http.StripPrefix("/dist", http.FileServer(http.Dir(staticFileDirName))))
+	r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, fmt.Sprintf("%s/index.html", staticFileDirName))
 	})
 
 	http.ListenAndServe(":8080", r)
---
+++ website/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>Babblegraph.com is currently under construction</h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('content'));
