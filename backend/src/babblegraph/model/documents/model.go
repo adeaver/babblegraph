f90aa0806af5448ae15fc63992f3fdf7eb2210d1
Andrew Deaver <deaver.andrew@gmail.com>
2020-12-29 12:35:06 -0600

    New model

--- backend/src/babblegraph/model/documents/model.go
+++ backend/src/babblegraph/model/documents/model.go
@@ -10,6 +10,7 @@ type Version int64
 const (
 	Version1 Version = 1
 	Version2 Version = 2
+	Version3 Version = 3
 
 	CurrentDocumentVersion Version = Version2
 )
@@ -43,14 +44,32 @@ type Metadata struct {
 type DocumentID string
 
 type Document struct {
-	ID               DocumentID             `json:"id"`
-	Version          Version                `json:"version"`
-	URL              string                 `json:"url"`
-	ReadabilityScore int64                  `json:"readability_score"`
-	LanguageCode     wordsmith.LanguageCode `json:"language_code"`
-	DocumentType     Type                   `json:"document_type"`
-	Metadata         Metadata               `json:"metadata"`
-	Domain           string                 `json:"domain"`
+	ID           DocumentID             `json:"id"`
+	Version      Version                `json:"version"`
+	URL          string                 `json:"url"`
+	LanguageCode wordsmith.LanguageCode `json:"language_code"`
+	DocumentType Type                   `json:"document_type"`
+	Metadata     Metadata               `json:"metadata"`
+	Domain       string                 `json:"domain"`
 
-	LemmatizedBodyDEPRECATED *string `json:"lemmatized_body,omitempty"`
+	ReadabilityScore  int64              `json:"readability_score"`
+	WordStatsVersion1 *WordStatsVersion1 `json:"word_stats_version_1,omitempty"`
+	Labels            []string           `json:"labels,omitempty"`
+}
+
+type WordStatsVersion1 struct {
+	AverageWordRanking  int64 `json:"average_word_ranking"`
+	MedianWordRanking   int64 `json:"median_word_ranking"`
+	TotalNumberOfWords  int64 `json:"total_number_of_words"` // Includes repeats
+	NumberOfUniqueWords int64 `json:"number_of_unique_words"`
+
+	LeastFrequentWordRanking         int64         `json:"least_frequent_word_ranking"`
+	LeastFrequentWordExclusion       WordExclusion `json:"least_frequent_word_exclusion"`
+	SecondLeastFrequentWordExclusion WordExclusion `json:"second_least_frequent_word_exclusion"`
+	ThirdLeastFrequentWordExclusion  WordExclusion `json:"third_least_frequent_word_exclusion"`
+}
+
+type WordExclusion struct {
+	LeastFrequentRankingWithoutWord int64  `json:"least_frequent_ranking_without_word"`
+	WordText                        string `json:"word_text"`
 }
