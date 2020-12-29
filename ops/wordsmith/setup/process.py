6ae80b53a0c4345801ac247b1dbfa0ffcbe5cf57
Andrew Deaver <deaver.andrew@gmail.com>
2020-12-29 12:35:36 -0600

    Rework initial wordsmith data

--- ops/wordsmith/setup/process.py
+++ ops/wordsmith/setup/process.py
@@ -1,12 +1,14 @@
 import io, uuid
 from corpus_read_defs import corpus_reader
 from templates import SQLTemplate
+from heapq import heapify
+from util import make_word_ranking_id
 
 LANGUAGE = "es"
-CORPUS = ("upc-wiki-corpus", uuid.uuid4())
+CORPUS = ("upc-wiki-corpus", "escrp1upc-wiki-corpus")
 
 print("reading data")
-observed_lemmas, observed_words, observed_parts_of_speech,  part_of_speech_trigram_counts, word_part_of_speech_counts = corpus_reader.read_data()
+observed_lemmas, observed_words, observed_parts_of_speech, word_text_counts  = corpus_reader.read_data()
 
 # HELPER FUNCTIONS
 def _word_data_for_word_key(word_key):
@@ -37,18 +39,26 @@ def _make_word_generator():
         part_of_speech_id = observed_parts_of_speech[part_of_speech]
         yield "{},{},{},{},{},{}".format(_id, LANGUAGE, CORPUS[1], part_of_speech_id, lemma_id, word_text)
 
-def _make_trigram_count_generator():
-    for trigram_key in part_of_speech_trigram_counts:
-        count = part_of_speech_trigram_counts[trigram_key]
-        parts_of_speech = trigram_key.split(",")
-        part_of_speech_ids = [observed_parts_of_speech[part_of_speech] for part_of_speech in parts_of_speech]
-        yield "{},{},{},{},{},{},{}".format(uuid.uuid4(), LANGUAGE, CORPUS[1], part_of_speech_ids[0], part_of_speech_ids[1], part_of_speech_ids[2], count)
+#  def _make_trigram_count_generator():
+#      for trigram_key in part_of_speech_trigram_counts:
+#          count = part_of_speech_trigram_counts[trigram_key]
+#          parts_of_speech = trigram_key.split(",")
+#          part_of_speech_ids = [observed_parts_of_speech[part_of_speech] for part_of_speech in parts_of_speech]
+#          yield "{},{},{},{},{},{},{}".format(uuid.uuid4(), LANGUAGE, CORPUS[1], part_of_speech_ids[0], part_of_speech_ids[1], part_of_speech_ids[2], count)
+#
+#  def _make_word_part_of_speech_count_generator():
+#      for word_key in word_part_of_speech_counts:
+#          count = word_part_of_speech_counts[word_key]
+#          _, _, word_id, _ = _word_data_for_word_key(word_key)
+#          yield "{},{},{},{},{}".format(uuid.uuid4(), LANGUAGE, CORPUS[1], word_id, count)
 
-def _make_word_part_of_speech_count_generator():
-    for word_key in word_part_of_speech_counts:
-        count = word_part_of_speech_counts[word_key]
-        _, _, word_id, _ = _word_data_for_word_key(word_key)
-        yield "{},{},{},{},{}".format(uuid.uuid4(), LANGUAGE, CORPUS[1], word_id, count)
+def _make_word_text_ranking_generator():
+    word_rankings = [(count, word_text) for word_text, count in word_text_counts.items()]
+    heapify(word_rankings)
+    for idx in range(len(word_rankings)):
+        word = word_rankings[idx][1]
+        ranking = len(word_rankings) - idx
+        yield "{},{},{}".format(make_word_ranking_id(word), word, ranking)
 
 part_of_speech_template = SQLTemplate(
     "parts_of_speech",
@@ -77,25 +87,35 @@ word_template.write_files_for_template(
     300000
 )
 
-part_of_speech_trigram_template =  SQLTemplate(
-    "part_of_speech_trigram_counts",
-    "_id,language,corpus_id,first_token_id,second_token_id,third_token_id,occurrences"
+word_ranking_template = SQLTemplate(
+    "word_rankings",
+    "_id,word,ranking"
 )
-print("writing part of speech trigram files")
-part_of_speech_trigram_template.write_files_for_template(
-    _make_trigram_count_generator,
+print("writing word ranking files")
+word_ranking_template.write_files_for_template(
+    _make_word_text_ranking_generator,
     300000
 )
 
-word_part_of_speech_template = SQLTemplate(
-    "word_part_of_speech_counts",
-    "_id,language,corpus_id,word_id,occurrences"
-)
-print("writing word counts files")
-word_part_of_speech_template.write_files_for_template(
-    _make_word_part_of_speech_count_generator,
-    300000
-)
+#  part_of_speech_trigram_template =  SQLTemplate(
+#      "part_of_speech_trigram_counts",
+#      "_id,language,corpus_id,first_token_id,second_token_id,third_token_id,occurrences"
+#  )
+#  print("writing part of speech trigram files")
+#  part_of_speech_trigram_template.write_files_for_template(
+#      _make_trigram_count_generator,
+#      300000
+#  )
+
+#  word_part_of_speech_template = SQLTemplate(
+#      "word_part_of_speech_counts",
+#      "_id,language,corpus_id,word_id,occurrences"
+#  )
+#  print("writing word counts files")
+#  word_part_of_speech_template.write_files_for_template(
+#      _make_word_part_of_speech_count_generator,
+#      300000
+#  )
 
 print("writing sql file")
 with io.open("/out/populate_db.sql", "w", encoding="latin1") as f:
@@ -107,9 +127,11 @@ with io.open("/out/populate_db.sql", "w", encoding="latin1") as f:
         f.write(template)
     for template in word_template.yield_sql_template():
         f.write(template)
-    for template in part_of_speech_trigram_template.yield_sql_template():
+    for template in word_ranking_template.yield_sql_template():
         f.write(template)
-    for template in word_part_of_speech_template.yield_sql_template():
-        f.write(template)
+    #  for template in part_of_speech_trigram_template.yield_sql_template():
+    #      f.write(template)
+    #  for template in word_part_of_speech_template.yield_sql_template():
+    #      f.write(template)
 
 print("done!")
