8b3d63da4ddf6b2a4511aefa1bf2ebbba0444edf
Andrew Deaver <deaver.andrew@gmail.com>
2020-12-30 17:35:14 -0600

    Update scripts and migrations

--- deploy/wordsmith/migrations/0006_word_rankings.up.sql
+++ deploy/wordsmith/migrations/0006_word_rankings.up.sql
@@ -1,9 +1,11 @@
-CREATE TABLE parts_of_speech(
+CREATE TABLE word_rankings(
     _id TEXT NOT NULL,
     language TEXT NOT NULL REFERENCES languages(code),
     corpus_id TEXT NOT NULL REFERENCES corpora(_id),
 
-    code TEXT NOT NULL,
+    word TEXT NOT NULL,
+    ranking NUMERIC NOT NULL,
+    count NUMERIC NOT NULL,
 
     PRIMARY KEY (_id)
 );
