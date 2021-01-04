e66e715a4780897127eb381c1d5f9c90728f36e9
Andrew Deaver <deaver.andrew@gmail.com>
2021-01-04 12:39:47 -0600

    Adds initial data and country codes (#82)

---
+++ backend/src/babblegraph/model/domains/initial.go
@@ -0,0 +1,32 @@
+package domains
+
+import (
+	"babblegraph/model/contenttopics"
+	"babblegraph/util/urlparser"
+)
+
+var domainMap map[string]AllowableDomain
+
+func initializeDomainMap() {
+	domainMap = make(map[string]AllowableDomain)
+	for _, d := range allowableDomains {
+		domainMap[string(d.Domain)] = d
+	}
+}
+
+func IsURLAllowed(u urlparser.ParsedURL) bool {
+	if len(domainMap) == 0 {
+		initializeDomainMap()
+	}
+	_, ok := domainMap[u.Domain]
+	return ok
+}
+
+func GetSeedDomains() map[string][]contenttopics.ContentTopic {
+	out := make(map[string][]contenttopics.ContentTopic)
+	for _, d := range allowableDomains {
+		out[string(d.Domain)] = d.Topics
+	}
+	// TODO: add seed urls initial data
+	return out
+}
--- backend/src/babblegraph/util/geo/country_code.go
+++ backend/src/babblegraph/util/geo/country_code.go
@@ -1,3 +1,23 @@
 package geo
 
 type CountryCode string
+
+const (
+	CountryCodeArgentina    CountryCode = "AR"
+	CountryCodeChile        CountryCode = "CL"
+	CountryCodeColombia     CountryCode = "CO"
+	CountryCodeCostaRica    CountryCode = "CR"
+	CountryCodeElSalvador   CountryCode = "SV"
+	CountryCodeGuatemala    CountryCode = "GT"
+	CountryCodeHonduras     CountryCode = "HN"
+	CountryCodeMexico       CountryCode = "MX"
+	CountryCodeNicaragua    CountryCode = "NI"
+	CountryCodePanama       CountryCode = "PA"
+	CountryCodePeru         CountryCode = "PE"
+	CountryCodeParaguay     CountryCode = "PY"
+	CountryCodeSpain        CountryCode = "ES"
+	CountryCodeUnitedStates CountryCode = "US"
+	CountryCodeVenezuela    CountryCode = "VE"
+	CountryCodeUruguay      CountryCode = "UY"
+	CountryCodePuertoRico   CountryCode = "PR"
+)
