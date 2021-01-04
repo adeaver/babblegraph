b0ed57172390bb4087868a5f92f7ee99ed6f628e
Andrew Deaver <deaver.andrew@gmail.com>
2021-01-04 14:16:03 -0600

    I think this works

--- backend/src/babblegraph/model/domains/initial.go
+++ backend/src/babblegraph/model/domains/initial.go
@@ -3,11 +3,21 @@ package domains
 import (
 	"babblegraph/model/contenttopics"
 	"babblegraph/util/urlparser"
+	"fmt"
+	"sync"
 )
 
-var domainMap map[string]AllowableDomain
+var (
+	domainMap        map[string]AllowableDomain
+	initializerMutex sync.Mutex
+)
 
 func initializeDomainMap() {
+	initializerMutex.Lock()
+	defer initializerMutex.Unlock()
+	if len(domainMap) != 0 {
+		return
+	}
 	domainMap = make(map[string]AllowableDomain)
 	for _, d := range allowableDomains {
 		domainMap[string(d.Domain)] = d
@@ -22,7 +32,18 @@ func IsURLAllowed(u urlparser.ParsedURL) bool {
 	return ok
 }
 
-func GetSeedDomains() map[string][]contenttopics.ContentTopic {
+func GetDomainMetadata(d string) (*AllowableDomain, error) {
+	if len(domainMap) == 0 {
+		initializeDomainMap()
+	}
+	metadata, ok := domainMap[d]
+	if !ok {
+		return nil, fmt.Errorf("Invalid domain")
+	}
+	return &metadata, nil
+}
+
+func GetSeedURLs() map[string][]contenttopics.ContentTopic {
 	out := make(map[string][]contenttopics.ContentTopic)
 	for _, d := range allowableDomains {
 		out[string(d.Domain)] = d.Topics
