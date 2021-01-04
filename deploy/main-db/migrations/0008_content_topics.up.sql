bae3c82ab920acbe50a6ab0eba0dcafd4ef230b0
Andrew Deaver <deaver.andrew@gmail.com>
2021-01-04 14:51:04 -0600

    Fix

--- deploy/main-db/migrations/0008_content_topics.up.sql
+++ deploy/main-db/migrations/0008_content_topics.up.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS content_topic_mappings(
     _id uuid DEFAULT uuid_generate_v4 (),
     url_identifier TEXT NOT NULL,
-    content_topic TEXT NOT NULL,
+    content_topic TEXT NOT NULL
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS content_topic_mapping_uniqueness_idx ON content_topic_mappings(url_identifier, content_topic);
