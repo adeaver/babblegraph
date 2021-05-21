e382afe956f91451f149a0ffcc9363295b843395
Andrew Deaver <deaver.andrew@gmail.com>
2021-05-21 14:48:21 -0700

    Add paywall calculation to ingestion (#210)

--- backend/src/babblegraph/services/worker/ingesthtml/util.go
+++ backend/src/babblegraph/services/worker/ingesthtml/util.go
@@ -1,6 +1,7 @@
 package ingesthtml
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 
@@ -69,4 +70,36 @@ func getKeyValuePairFromMetaTag(n *html.Node) (_key, _value *string) {
 		return nil, nil
 	}
 	return &name, &value
+}
+
+func processPaywallFromLDJSON(ldJSONData string) (bool, error) {
+	var ldJSON map[string]interface{}
+	if err := json.Unmarshal([]byte(ldJSONData), &ldJSON); err != nil {
+		return false, err
+	}
+	isAccessibleInterface, ok := ldJSON["isAccessibleForFree"]
+	if !ok {
+		return false, fmt.Errorf("LD+JSON does not contain isAccessibleForFree, assuming not paywalled")
+	}
+	isAccessibleForFree, ok := isAccessibleInterface.(bool)
+	if !ok {
+		return false, fmt.Errorf("Could not convert isAccessibleForFree key to bool")
+	}
+	return !isAccessibleForFree, nil
+}
+
+func processPaywallFromClasses(node *html.Node, paywallValidationClasses []string) bool {
+	for _, attr := range node.Attr {
+		if attr.Key == "class" {
+			classes := strings.Split(attr.Val, " ")
+			for _, c := range classes {
+				for _, paywallClass := range paywallValidationClasses {
+					if c == paywallClass {
+						return true
+					}
+				}
+			}
+		}
+	}
+	return false
 }
