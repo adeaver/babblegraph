eb1d5aab54610a8b2e81c04afa78b8a1d30c845e
Andrew Deaver <deaver.andrew@gmail.com>
2021-06-14 15:53:42 -0400

    login page

--- backend/src/babblegraph/services/web/middleware/auth.go
+++ backend/src/babblegraph/services/web/middleware/auth.go
@@ -58,7 +58,6 @@ func WithAuthorizationCheck(w http.ResponseWriter, r *http.Request, input WithAu
 				input.HandleNoUserFound(w, r)
 				return
 			default:
-				var userSubscriptionLevel *useraccounts.SubscriptionLevel
 				if err := database.WithTx(func(tx *sqlx.Tx) error {
 					var err error
 					userSubscriptionLevel, err = useraccounts.LookupSubscriptionLevelForUser(tx, *userID)
@@ -79,49 +78,28 @@ func WithAuthorizationCheck(w http.ResponseWriter, r *http.Request, input WithAu
 
 func WithAuthorizationLevelVerification(validAuthorizationLevels []useraccounts.SubscriptionLevel, fn func(userID users.UserID) func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var userSubscriptionLevel *useraccounts.SubscriptionLevel
-		var userID *users.UserID
-		for _, cookie := range r.Cookies() {
-			if cookie.Name == authTokenCookieName {
-				var err error
-				var isValid bool
-				token := cookie.Value
-				userID, isValid, err = auth.VerifyJWTAndGetUserID(token)
-				switch {
-				case err != nil:
-					log.Println(fmt.Sprintf("Error authenticating user: %s", err.Error()))
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				case !isValid:
-					// Redirect to login page
-				case userID == nil:
-					log.Println(fmt.Sprintf("Error authenticating user: No user"))
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				default:
-					if err := database.WithTx(func(tx *sqlx.Tx) error {
-						var err error
-						userSubscriptionLevel, err = useraccounts.LookupSubscriptionLevelForUser(tx, *userID)
-						return err
-					}); err != nil {
-						log.Println(fmt.Sprintf("Error authenticating user: %s", err.Error()))
-						w.WriteHeader(http.StatusBadRequest)
+		WithAuthorizationCheck(w, r, WithAuthorizationCheckInput{
+			HandleFoundSubscribedUser: func(userID users.UserID, subscriptionLevel useraccounts.SubscriptionLevel, w http.ResponseWriter, r *http.Request) {
+				for _, validSubscriptionLevel := range validAuthorizationLevels {
+					if subscriptionLevel == validSubscriptionLevel {
+						fn(userID)(w, r)
 						return
 					}
-					break
 				}
-			}
-		}
-		if userSubscriptionLevel == nil {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-		for _, validSubscriptionLevel := range validAuthorizationLevels {
-			if *userSubscriptionLevel == validSubscriptionLevel {
-				fn(*userID)(w, r)
-				return
-			}
-		}
-		w.WriteHeader(http.StatusUnauthorized)
+				w.WriteHeader(http.StatusUnauthorized)
+			},
+			HandleNoUserFound:                HandleUnauthorizedRequest,
+			HandleInvalidAuthenticationToken: HandleUnauthorizedRequest,
+			HandleError:                      HandleAuthorizationError,
+		})
 	}
+}
+
+func HandleUnauthorizedRequest(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusUnauthorized)
+}
+
+func HandleAuthorizationError(err error, w http.ResponseWriter, r *http.Request) {
+	log.Println(fmt.Sprintf("Error authenticating user: %s", err.Error()))
+	w.WriteHeader(http.StatusBadRequest)
 }
