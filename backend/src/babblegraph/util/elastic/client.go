68a5bb263ef6ca2887e5b372ecf606c8864aefdd
Andrew Deaver <deaver.andrew@gmail.com>
2021-07-24 11:59:15 -0400

    Migration Helper (#261)

--- backend/src/babblegraph/util/elastic/client.go
+++ backend/src/babblegraph/util/elastic/client.go
@@ -14,16 +14,23 @@ const (
 	elasticsearchHostsKey    string = "ES_HOSTS"
 	elasticsearchUsernameKey string = "ES_USERNAME"
 	elasticsearchPasswordKey string = "ES_PASSWORD"
+
+	elasticsearchMigrationHostsKey    string = "ES_MIGRATION_HOSTS"
+	elasticsearchMigrationUsernameKey string = "ES_MIGRATION_USERNAME"
+	elasticsearchMigrationPasswordKey string = "ES_MIGRATION_PASSWORD"
 )
 
-var esClient *elasticsearch.Client
+var (
+	esClient        *elasticsearch.Client
+	migrationClient *elasticsearch.Client
+)
 
 func InitializeElasticsearchClientForEnvironment() error {
 	if esClient != nil {
 		panic("elasticsearch client is already initialized")
 	}
 	cfg := elasticsearch.Config{
-		Addresses: getAddressesForEnvironment(),
+		Addresses: getAddressesForEnvironment(elasticsearchHostsKey),
 		Username:  env.GetEnvironmentVariableOrDefault(elasticsearchUsernameKey, "elastic"),
 		Password:  env.MustEnvironmentVariable(elasticsearchPasswordKey),
 		Transport: &http.Transport{
@@ -41,10 +48,31 @@ func InitializeElasticsearchClientForEnvironment() error {
 	}
 	var err error
 	esClient, err = elasticsearch.NewClient(cfg)
+	if err != nil {
+		return err
+	}
+	migrationConfig := elasticsearch.Config{
+		Addresses: getAddressesForEnvironment(elasticsearchMigrationHostsKey),
+		Username:  env.GetEnvironmentVariableOrDefault(elasticsearchMigrationUsernameKey, "elastic"),
+		Password:  env.MustEnvironmentVariable(elasticsearchMigrationPasswordKey),
+		Transport: &http.Transport{
+			DialContext: (&net.Dialer{
+				KeepAlive: 60 * time.Second,
+				DualStack: true,
+				Timeout:   15 * time.Second,
+			}).DialContext,
+			MaxIdleConns:          2,
+			IdleConnTimeout:       0,
+			TLSHandshakeTimeout:   0,
+			MaxIdleConnsPerHost:   0,
+			ExpectContinueTimeout: 5 * time.Second,
+		},
+	}
+	migrationClient, err = elasticsearch.NewClient(migrationConfig)
 	return err
 }
 
-func getAddressesForEnvironment() []string {
-	addressesUnsplit := env.MustEnvironmentVariable(elasticsearchHostsKey)
+func getAddressesForEnvironment(hostsKey string) []string {
+	addressesUnsplit := env.MustEnvironmentVariable(hostsKey)
 	return strings.Split(addressesUnsplit, ",")
 }
--- backend/src/babblegraph/util/elastic/index.go
+++ backend/src/babblegraph/util/elastic/index.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	"github.com/elastic/go-elasticsearch/esapi"
+	"github.com/getsentry/sentry-go"
 )
 
 type Index interface {
@@ -38,10 +39,17 @@ func CreateIndex(index Index, settings *CreateIndexSettings) error {
 	res, err := createIndexRequest.Do(context.Background(), esClient)
 	if err != nil {
 		log.Println(fmt.Sprintf("Caught error creating: %s", err.Error()))
-		return err
+		return nil
 	}
 	defer res.Body.Close()
 	log.Println(res)
+	migrationRes, err := createIndexRequest.Do(context.Background(), migrationClient)
+	if err != nil {
+		sentry.CaptureException(fmt.Errorf("Caught error creating index for migration stack: %s", err.Error()))
+		return nil
+	}
+	defer migrationRes.Body.Close()
+	log.Println(migrationRes)
 	return nil
 }
 
@@ -69,5 +77,22 @@ func IndexDocument(index Index, document interface{}) error {
 	}
 	defer res.Body.Close()
 	log.Println(res)
+	indexRequest = esapi.IndexRequest{
+		Index:      index.GetName(),
+		Body:       strings.NewReader(string(documentAsJSON)),
+		DocumentID: *docID,
+		Refresh:    "true",
+	}
+	migrationRes, err := indexRequest.Do(context.Background(), migrationClient)
+	if err != nil {
+		sentry.CaptureException(fmt.Errorf("Caught error indexing for migration stack: %s", err.Error()))
+		return nil
+	}
+	defer migrationRes.Body.Close()
+	if migrationRes.StatusCode >= 300 {
+		sentry.CaptureException(fmt.Errorf("Got status code %d for migration: %+v", migrationRes.StatusCode, migrationRes))
+		return nil
+	}
+	log.Println(migrationRes)
 	return nil
 }
