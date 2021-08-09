6d80fb49a78e5e7606cabec97976d09cc94546fe
Andrew Deaver <deaver.andrew@gmail.com>
2021-08-09 16:25:28 -0400

    Can add payments

--- website/src/components/SubscriptionManagement/PaymentAndSubscriptionPage.tsx
+++ website/src/components/SubscriptionManagement/PaymentAndSubscriptionPage.tsx
@@ -5,6 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 
+import AddPaymentMethodForm from 'common/components/Stripe/AddPaymentMethodForm';
 import LoadingSpinner from 'common/components/LoadingSpinner/LoadingSpinner';
 import Page from 'common/components/Page/Page';
 import DisplayCard from 'common/components/DisplayCard/DisplayCard';
@@ -17,6 +18,8 @@ import CardIcon from 'common/components/Payment/CardIcon';
 import { ContentHeader } from './common';
 
 import {
+    getPaymentMethodByID,
+    GetPaymentMethodByIDResponse,
     getPaymentMethodsForUser,
     GetPaymentMethodsForUserResponse,
     PaymentMethod
@@ -41,6 +44,8 @@ const PaymentAndSubscriptionPage = (props: PaymentAndSubscriptionPageProps) => {
     const [ paymentMethods, setPaymentMethods ] = useState<Array<PaymentMethod>>([]);
     const [ error, setError ] = useState<Error>(null);
 
+    const [ isLoadingStripeRequest, setIsLoadingStripeRequest ] = useState<boolean>(false);
+
     useEffect(() => {
         getPaymentMethodsForUser({},
             (resp: GetPaymentMethodsForUserResponse) => {
@@ -52,6 +57,20 @@ const PaymentAndSubscriptionPage = (props: PaymentAndSubscriptionPageProps) => {
                 setError(err);
             });
     }, []);
+
+    const handleSuccessfullyAddedPaymentMethod = (paymentMethodID: string) => {
+        getPaymentMethodByID({
+            stripePaymentMethodId: paymentMethodID,
+        },
+        (resp: GetPaymentMethodByIDResponse) => {
+            setIsLoadingStripeRequest(false);
+            resp.paymentMethod && setPaymentMethods(paymentMethods.concat(resp.paymentMethod));
+        },
+        (err: Error) => {
+            setIsLoadingStripeRequest(false);
+            setError(err);
+        })
+    }
 
     const isLoading = isLoadingPaymentMethods;
     let body;
@@ -67,7 +86,11 @@ const PaymentAndSubscriptionPage = (props: PaymentAndSubscriptionPageProps) => {
         body = (
             <div>
                 <PaymentMethodsDisplay
-                    paymentMethods={paymentMethods} />
+                    paymentMethods={paymentMethods}
+                    isLoadingStripeRequest={isLoadingStripeRequest}
+                    handleIsLoadingStripeRequest={setIsLoadingStripeRequest}
+                    handlePaymentMethodAddedSuccess={handleSuccessfullyAddedPaymentMethod}
+                    handleAddPaymentMethodError={setError} />
             </div>
         )
     }
@@ -92,9 +115,22 @@ const PaymentAndSubscriptionPage = (props: PaymentAndSubscriptionPageProps) => {
 
 type PaymentMethodsDisplayProps = {
     paymentMethods: Array<PaymentMethod>;
+    isLoadingStripeRequest: boolean;
+
+    handleIsLoadingStripeRequest: (isLoading: boolean) => void;
+    handlePaymentMethodAddedSuccess: (paymentMethodID: string) => void;
+    handleAddPaymentMethodError: (err: Error) => void;
 }
 
 const PaymentMethodsDisplay = (props: PaymentMethodsDisplayProps) => {
+    const [ showAddPaymentMethodForm, setShowAddPaymentMethodForm ] = useState<boolean>(false);
+    const [ addPaymentMethodFailure, setAddPaymentMethodFailure ] = useState<string | null>(null);
+
+    const handlePaymentMethodAddedSuccess = (paymentMethodID: string) => {
+        props.handlePaymentMethodAddedSuccess(paymentMethodID);
+        setShowAddPaymentMethodForm(false);
+    }
+
     const paymentMethods = props.paymentMethods.map((p: PaymentMethod) => (
         <PaymentMethodDisplay key={p.stripePaymentMethodId} {...p} />
     ));
@@ -103,9 +139,31 @@ const PaymentMethodsDisplay = (props: PaymentMethodsDisplayProps) => {
             <Heading3 color={TypographyColor.Primary}>
                 Your Payment Methods
             </Heading3>
-            <Grid container>
-                { paymentMethods }
-            </Grid>
+            {
+                showAddPaymentMethodForm ? (
+                    <AddPaymentMethodForm
+                        handleIsStripeRequestLoading={props.handleIsLoadingStripeRequest}
+                        handleSuccess={handlePaymentMethodAddedSuccess}
+                        handleFailure={setAddPaymentMethodFailure}
+                        handleError={props.handleAddPaymentMethodError} />
+
+                ) : (
+                    <PrimaryButton
+                        disabled={props.isLoadingStripeRequest}
+                        onClick={() => setShowAddPaymentMethodForm(true)}>
+                        Add new payment method
+                    </PrimaryButton>
+                )
+            }
+            {
+                props.isLoadingStripeRequest ? (
+                    <LoadingSpinner />
+                ) : (
+                    <Grid container spacing={2}>
+                        { paymentMethods }
+                    </Grid>
+                )
+            }
         </div>
     );
 }
@@ -123,14 +181,18 @@ const PaymentMethodDisplay = (props: PaymentMethod) => {
                     Expires { props.expirationMonth }/{ props.expirationYear }
                 </Paragraph>
                 <Grid container>
-                    <Grid item xs={12} md={6}>
-                        <PrimaryButton>
-                            Set as default
-                        </PrimaryButton>
-                    </Grid>
+                    {
+                        !props.isDefault && (
+                            <Grid item xs={12} md={6}>
+                                <PrimaryButton>
+                                    Make default
+                                </PrimaryButton>
+                            </Grid>
+                        )
+                    }
                     <Grid item xs={12} md={6}>
                         <WarningButton>
-                            Delete Payment Method
+                            Delete
                         </WarningButton>
                     </Grid>
                 </Grid>
