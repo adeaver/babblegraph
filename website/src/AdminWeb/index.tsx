5fcd4b539a5d1835e9abcc5566feb2875f6e2663
Andrew Deaver <deaver.andrew@gmail.com>
2021-11-19 15:57:16 -0500

    Start dashboard

--- website/src/AdminWeb/index.tsx
+++ website/src/AdminWeb/index.tsx
@@ -9,12 +9,15 @@ import {
 
 import RegistrationPage from 'AdminWeb/components/RegistrationPage/RegistrationPage';
 import LoginPage from 'AdminWeb/components/LoginPage/LoginPage';
+import Dashboard from 'AdminWeb/components/Dashboard/Dashboard';
 
 class App extends React.Component{
     render() {
         return (
             <Router basename="/ops">
                 <Switch>
+                    <Route path="/dashboard" component={Dashboard} />
+
                     <Route path="/register/:token" component={RegistrationPage} />
                     <Route component={LoginPage} />
                 </Switch>
