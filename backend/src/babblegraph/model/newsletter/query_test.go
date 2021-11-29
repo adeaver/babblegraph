6910ba4fcfde610566bdc58c2c4e77b09bbb3bdd
Andrew Deaver <deaver.andrew@gmail.com>
2021-11-29 12:35:02 -0600

    Add test

--- backend/src/babblegraph/model/newsletter/query_test.go
+++ backend/src/babblegraph/model/newsletter/query_test.go
@@ -2,14 +2,18 @@ package newsletter
 
 import (
 	"babblegraph/model/contenttopics"
+	"babblegraph/model/documents"
+	"babblegraph/model/email"
 	"babblegraph/model/routes"
 	"babblegraph/model/useraccounts"
+	"babblegraph/model/userlemma"
 	"babblegraph/model/usernewsletterschedule"
 	"babblegraph/util/ptr"
 	"babblegraph/util/testutils"
 	"babblegraph/wordsmith"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestUserHasAccount(t *testing.T) {
@@ -141,5 +145,84 @@ func TestUserScheduleDayNoSubscription(t *testing.T) {
 	}
 	if testNewsletter == nil {
 		t.Errorf("Expected non-null newsletter, but it was not")
+	}
+}
+
+func TestSpotlightRecordsForUserWithAccount(t *testing.T) {
+	emailAccessor := getTestEmailAccessor()
+	userAccessor := &testUserAccessor{
+		languageCode:        wordsmith.LanguageCodeSpanish,
+		doesUserHaveAccount: true,
+		userTopics: []contenttopics.ContentTopic{
+			contenttopics.ContentTopicArt,
+		},
+		readingLevel: &userReadingLevel{
+			LowerBound: 30,
+			UpperBound: 80,
+		},
+		spotlightRecords: []userlemma.UserLemmaReinforcementSpotlightRecord{
+			{
+				LanguageCode: wordsmith.LanguageCodeSpanish,
+				LemmaID:      "word1",
+				LastSentOn:   time.Now(),
+			}, {
+				LanguageCode: wordsmith.LanguageCodeSpanish,
+				LemmaID:      "word2",
+				LastSentOn:   time.Now(),
+			}, {
+				LanguageCode: wordsmith.LanguageCodeSpanish,
+				LemmaID:      "word3",
+				LastSentOn:   time.Now().Add(-7 * 24 * time.Hour),
+			},
+		},
+	}
+	var links []Link
+	emailRecordID := email.NewEmailRecordID()
+	lemmasByID := make(map[wordsmith.LemmaID]wordsmith.Lemma)
+	lemmas := []wordsmith.LemmaID{"otherWord", "otherWord1", "word1", "word2", "word3", "word4"}
+	var docs []documents.DocumentWithScore
+	for idx, lemma := range lemmas {
+		lemmasByID[lemma] = wordsmith.Lemma{
+			ID:        lemma,
+			Language:  wordsmith.LanguageCodeSpanish,
+			LemmaText: lemma.Str(),
+		}
+		doc, link, err := getDefaultDocumentWithLink(idx, emailRecordID, userAccessor, getDefaultDocumentInput{
+			Topics:                 []contenttopics.ContentTopic{contenttopics.ContentTopicArt},
+			SeedJobIngestTimestamp: ptr.Int64(time.Now().Add(-1 * time.Duration(idx) * 24 * time.Hour).Unix()),
+			Lemmas:                 []wordsmith.LemmaID{lemma},
+		})
+		if err != nil {
+			t.Fatalf("Error setting up test: %s", err.Error())
+		}
+		docs = append(docs, *doc)
+		links = append(links, *link)
+	}
+	wordsmithAccessor := &testWordsmithAccessor{
+		lemmasByID: lemmasByID,
+	}
+	docsAccessor := &testDocsAccessor{documents: docs}
+	testNewsletter, err := CreateNewsletter(wordsmithAccessor, emailAccessor, userAccessor, docsAccessor)
+	switch {
+	case err != nil:
+		t.Fatalf(err.Error())
+	case testNewsletter == nil:
+		t.Errorf("Expected non-null newsletter, but it was not")
+	case testNewsletter.Body.LemmaReinforcementSpotlight == nil:
+		t.Errorf("Expected non-null newsletter lemma rei, but it was not")
+	default:
+		if testNewsletter.Body.LemmaReinforcementSpotlight.LemmaText != "word3" {
+			t.Errorf("Expected lemma to be word3, but got %s", testNewsletter.Body.LemmaReinforcementSpotlight.LemmaText)
+		}
+		correctDocument, err := testLink(testNewsletter.Body.LemmaReinforcementSpotlight.Document, links[4])
+		if !correctDocument {
+			t.Errorf("Document ID from links is not correct")
+		}
+		if err != nil {
+			t.Errorf("Error comparing links: %s", err.Error())
+		}
+		if !strings.HasSuffix(testNewsletter.Body.LemmaReinforcementSpotlight.PreferencesLink, "login?d=npf") {
+			t.Errorf("Expected link to end with login?d=npf, but got %s", testNewsletter.Body.LemmaReinforcementSpotlight.PreferencesLink)
+		}
 	}
 }
