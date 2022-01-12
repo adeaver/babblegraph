1158e501b1401fd236d4198b35cfb0f35aead5a5
Andrew Deaver <deaver.andrew@gmail.com>
2022-01-12 18:02:39 -0500

    Add content models

---
+++ backend/src/babblegraph/model/content/model.go
@@ -0,0 +1,161 @@
+package content
+
+import (
+	"babblegraph/util/geo"
+	"babblegraph/wordsmith"
+	"strings"
+)
+
+type TopicID string
+
+type dbTopic struct {
+	ID       TopicID `db:"_id"`
+	Label    string  `db:"label"`
+	IsActive bool    `db:"is_active"`
+}
+
+func (d dbTopic) ToNonDB() Topic {
+	return Topic{
+		ID:       d.ID,
+		Label:    d.Label,
+		IsActive: d.IsActive,
+	}
+}
+
+type Topic struct {
+	ID       TopicID `json:"id"`
+	Label    string  `json:"label"`
+	IsActive bool    `json:"is_active"`
+}
+
+type SourceID string
+
+type dbSource struct {
+	ID                 SourceID               `db:"_id"`
+	URL                string                 `db:"url"`
+	Type               SourceType             `db:"type"`
+	Country            geo.CountryCode        `db:"country"`
+	IngestStrategy     IngestStrategy         `db:"ingest_strategy"`
+	LanguageCode       wordsmith.LanguageCode `db:"language_code"`
+	IsActive           bool                   `db:"is_active"`
+	MonthlyAccessLimit *int64                 `json:"monthly_access_limit"`
+}
+
+func (d dbSource) ToNonDB() Source {
+	return Source{
+		ID:                 d.ID,
+		URL:                d.URL,
+		Type:               d.Type,
+		Country:            d.Country,
+		IngestStrategy:     d.IngestStrategy,
+		LanguageCode:       d.LanguageCode,
+		IsActive:           d.IsActive,
+		MonthlyAccessLimit: d.MonthlyAccessLimit,
+	}
+}
+
+type Source struct {
+	ID                 SourceID               `json:"id"`
+	URL                string                 `json:"url"`
+	Type               SourceType             `json:"type"`
+	Country            geo.CountryCode        `json:"country"`
+	IngestStrategy     IngestStrategy         `json:"ingest_strategy"`
+	LanguageCode       wordsmith.LanguageCode `json:"language_code"`
+	IsActive           bool                   `json:"is_active"`
+	MonthlyAccessLimit *int64                 `json:"monthly_access_limit"`
+}
+
+type SourceType string
+
+const (
+	SourceTypeNewsWebsite SourceType = "news-website"
+)
+
+type IngestStrategy string
+
+const (
+	IngestStrategyWebsiteHTML1 IngestStrategy = "website-html-1"
+)
+
+type SourceSeedID string
+
+type dbSourceSeed struct {
+	ID       SourceSeedID `json:"id"`
+	RootID   SourceID     `json:"root_id"`
+	URL      string       `json:"url"`
+	IsActive bool         `json:"is_active"`
+}
+
+func (d dbSourceSeed) ToNonDB() SourceSeed {
+	return SourceSeed{
+		ID:       d.ID,
+		RootID:   d.RootID,
+		URL:      d.URL,
+		IsActive: d.IsActive,
+	}
+}
+
+type SourceSeed struct {
+	ID       SourceSeedID `json:"id"`
+	RootID   SourceID     `json:"root_id"`
+	URL      string       `json:"url"`
+	IsActive bool         `json:"is_active"`
+}
+
+type SourceFilterID string
+
+const paywallFilterDelimiter = "#"
+
+type dbSourceFilter struct {
+	ID                  SourceFilterID `db:"_id"`
+	RootID              SourceID       `db:"root_id"`
+	IsActive            bool           `db:"is_active"`
+	UseLDJSONValidation bool           `db:"use_ld_db_validation"`
+	PaywallClasses      string         `db:"paywall_classes"`
+	PaywallIDs          string         `db:"paywall_ids"`
+}
+
+func (d dbSourceFilter) ToNonDB() SourceFilter {
+	return SourceFilter{
+		ID:                  d.ID,
+		RootID:              d.RootID,
+		IsActive:            d.IsActive,
+		UseLDJSONValidation: d.UseLDJSONValidation,
+		PaywallClasses:      strings.Split(d.PaywallClasses, paywallFilterDelimiter),
+		PaywallIDs:          strings.Split(d.PaywallIDs, paywallFilterDelimiter),
+	}
+}
+
+type SourceFilter struct {
+	ID                  SourceFilterID `json:"id"`
+	RootID              SourceID       `json:"root_id"`
+	IsActive            bool           `json:"is_active"`
+	UseLDJSONValidation bool           `json:"use_ld_json_validation"`
+	PaywallClasses      []string       `json:"paywall_classes"`
+	PaywallIDs          []string       `json:"paywall_ids"`
+}
+
+type SourceSeedTopicMappingID string
+
+type dbSourceSeedTopicMapping struct {
+	ID           SourceSeedTopicMappingID `db:"_id"`
+	SourceSeedID SourceSeedID             `db:"source_seed_id"`
+	TopicID      TopicID                  `db:"topic_id"`
+	IsActive     bool                     `db:"is_active"`
+}
+
+func (d dbSourceSeedTopicMapping) ToNonDB() SourceSeedTopicMapping {
+	return SourceSeedTopicMapping{
+		ID:           d.ID,
+		SourceSeedID: d.SourceSeedID,
+		TopicID:      d.TopicID,
+		IsActive:     d.IsActive,
+	}
+}
+
+type SourceSeedTopicMapping struct {
+	ID           SourceSeedTopicMappingID `json:"id"`
+	SourceSeedID SourceSeedID             `json:"source_seed_id"`
+	TopicID      TopicID                  `json:"topic_id"`
+	IsActive     bool                     `json:"is_active"`
+}
