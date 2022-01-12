14c4696cd0c577d9c8fe6150e09ab715f1d3acd4
Andrew Deaver <deaver.andrew@gmail.com>
2022-01-12 10:24:00 -0500

    Move signup component into separate file

--- website/src/ConsumerWeb/components/HomePage/HomePage.tsx
+++ website/src/ConsumerWeb/components/HomePage/HomePage.tsx
@@ -24,6 +24,8 @@ import Link, { LinkTarget } from 'common/components/Link/Link';
 import { withCaptchaToken, loadCaptchaScript } from 'common/util/grecaptcha/grecaptcha';
 import Form from 'common/components/Form/Form';
 
+import SignupForm from 'ConsumerWeb/components/common/SignupForm/SignupForm';
+
 import {
     SignupUserResponse,
     SignupErrorMessage,
@@ -86,43 +88,11 @@ const errorMessages = {
 
 }
 
-declare const window: any;
-
 const HomePage = () => {
-    const [ emailAddress, setEmailAddress ] = useState<string>('');
     const [ isLoading, setIsLoading ] = useState<boolean>(false);
-    const [ errorMessage, setErrorMessage ] = useState<string | null>(null);
     const [ hadSuccess, setHadSuccess ] = useState<boolean>(false);
     const [ hasLoadedCaptcha, setHasLoadedCaptcha ] = useState<boolean>(false);
 
-    const handleSubmit = () => {
-        setIsLoading(true);
-        withCaptchaToken("signup", (token: string) => {
-            signupUser({
-                emailAddress: emailAddress,
-                captchaToken: token,
-            },
-            (resp: SignupUserResponse) => {
-                setIsLoading(false);
-                if (!!resp.errorMessage) {
-                    setErrorMessage(errorMessages[resp.errorMessage] || errorMessages["default"]);
-                    setHadSuccess(false);
-                } else if (resp.success) {
-                    window.gtag_report_conversion(undefined);
-                    setErrorMessage(null);
-                    setHadSuccess(true);
-                } else {
-                    setErrorMessage(errorMessages["default"]);
-                    setHadSuccess(false);
-                }
-            },
-            (e: Error) => {
-                setIsLoading(false);
-                setErrorMessage(errorMessages["default"]);
-                setHadSuccess(false);
-            });
-        });
-    }
     useEffect(() => {
         loadCaptchaScript();
         setPageLoadEvent({},
@@ -131,27 +101,11 @@ const HomePage = () => {
         setHasLoadedCaptcha(true);
     }, []);
 
-    const classes = styleClasses();
-    let body;
-    if (isLoading) {
-        body = (<LoadingSpinner />);
-    } else if (hadSuccess) {
-        body = (
-            <SuccessConfirmation
-                emailAddress={emailAddress}
-                handleResendVerificationEmail={handleSubmit}
-                handleReturnHome={() => { setHadSuccess(false)}} />
-        );
-    } else {
-        body = (
-            <SignupForm
-                emailAddress={emailAddress}
-                errorMessage={errorMessage}
-                canSubmit={hasLoadedCaptcha}
-                handleSubmit={handleSubmit}
-                handleEmailAddressChange={setEmailAddress} />
-        );
+    const handleSuccess = (emailAddress: string) => {
+        setHadSuccess(true);
     }
+
+    const classes = styleClasses();
     return (
         <Page withBackground={PhotoKey.Seville}>
             <Grid container>
@@ -160,7 +114,44 @@ const HomePage = () => {
                 </Grid>
                 <Grid item xs={12} md={6}>
                     <Card className={classes.displayCard} variant='outlined'>
-                        {body}
+                        <Heading1 color={TypographyColor.Primary}>
+                            {
+                                !hadSuccess ? (
+                                    "Don’t lose your Spanish"
+                                ) : (
+                                    "Almost done! Just one last step."
+                                )
+                            }
+                        </Heading1>
+                        <Divider />
+                        {
+                            !hadSuccess && (
+                                <div>
+                                    <Paragraph>
+                                        Babblegraph picks up where your Spanish class left off by sending you a daily email with real articles from the Spanish-speaking world. You can even customize the content to make keeping up with your Spanish skills more engaging!
+                                    </Paragraph>
+                                    <Paragraph>
+                                        It’s completely free and you can unsubscribe anytime you’d like.
+                                    </Paragraph>
+                                </div>
+                            )
+                        }
+                        <SignupForm
+                            disabled={isLoading || !hasLoadedCaptcha}
+                            setIsLoading={setIsLoading}
+                            onSuccess={handleSuccess} />
+                        {
+                            isLoading ? (
+                                <LoadingSpinner />
+                            ) : (
+                                !hadSuccess ? (
+                                    <PostInitialContent />
+                                ) : (
+                                    <PostVerificationContent
+                                        handleReturnHome={() => setHadSuccess(false)} />
+                                )
+                            )
+                        }
                     </Card>
                     <Card className={classes.displayCard} variant='outlined'>
                         <Paragraph>
@@ -176,66 +167,10 @@ const HomePage = () => {
     );
 }
 
-type SignupFormProps = {
-    emailAddress: string;
-    canSubmit: boolean;
-    errorMessage: string | null;
-
-    handleSubmit: () => void;
-    handleEmailAddressChange: (emailAddress: string) => void;
-}
-
-const SignupForm = (props: SignupFormProps) => {
-    const handleEmailAddressChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        props.handleEmailAddressChange((event.target as HTMLInputElement).value);
-    };
-    const preventDefault = (event: React.SyntheticEvent) => event.preventDefault();
-
+const PostInitialContent = () => {
     const classes = styleClasses();
     return (
         <div>
-            <Heading1 color={TypographyColor.Primary}>
-                Don’t lose your Spanish
-            </Heading1>
-            <Divider />
-            <Paragraph>
-                Babblegraph picks up where your Spanish class left off by sending you a daily email with real articles from the Spanish-speaking world. You can even customize the content to make keeping up with your Spanish skills more engaging!
-            </Paragraph>
-            <Paragraph>
-                It’s completely free and you can unsubscribe anytime you’d like.
-            </Paragraph>
-            <Form className={classes.confirmationForm} handleSubmit={props.handleSubmit}>
-                <Grid container>
-                    <Grid item xs={9} md={10}>
-                        <PrimaryTextField
-                            id="email"
-                            className={classes.emailField}
-                            label="Email Address"
-                            variant="outlined"
-                            defaultValue={props.emailAddress}
-                            onChange={handleEmailAddressChange} />
-                    </Grid>
-                    <Grid item xs={3} md={2} className={classes.submitButtonContainer}>
-                        <PrimaryButton disabled={!props.emailAddress && props.canSubmit}>
-                            Try it!
-                        </PrimaryButton>
-                    </Grid>
-                </Grid>
-            </Form>
-            {
-                !!props.errorMessage && (
-                    <Grid container>
-                        <Grid className={classes.iconContainer} item xs={1}>
-                            <ErrorOutlineIcon className={classes.warningIcon} />
-                        </Grid>
-                        <Grid item xs={11}>
-                            <Paragraph size={Size.Small} color={TypographyColor.Warning}>
-                                {props.errorMessage}
-                            </Paragraph>
-                        </Grid>
-                    </Grid>
-                )
-            }
             <Link href="/privacy-policy">
                 View our Privacy Policy
             </Link>
@@ -277,52 +212,27 @@ const SignupForm = (props: SignupFormProps) => {
                 Learn more about Babblegraph
             </Link>
         </div>
-    )
+    );
 }
 
-type SuccessConfirmationProps = {
-    emailAddress: string;
-
-    handleResendVerificationEmail: () => void;
+type PostVerificationContentProps = {
     handleReturnHome: () => void;
-};
+}
 
-const SuccessConfirmation = (props: SuccessConfirmationProps) => {
+const PostVerificationContent = (props: PostVerificationContentProps) => {
     const classes = styleClasses();
     return (
-        <div>
-            <Heading1 color={TypographyColor.Primary}>Almost done! Just one last step.</Heading1>
-            <Paragraph>
-                Check your email for a verification email from hello@babblegraph.com. We sent it to {props.emailAddress}.
-            </Paragraph>
-            <Paragraph>
-                You’ll need to click the button in the verification email that was just sent to you in order to start receiving emails from Babblegraph.
-            </Paragraph>
-            <Paragraph>
-                It can take up to 5 minutes for the email to make its way to your inbox.
-            </Paragraph>
-            <Grid container>
-                <Grid item xs={3} md={4}>
-                    &nbsp;
-                </Grid>
-                <Grid item xs={6} md={4}>
-                    <PrimaryButton className={classes.verificationButton} onClick={props.handleResendVerificationEmail}>
-                        Resend the verification email
-                    </PrimaryButton>
-                </Grid>
+        <Grid container>
+            <Grid item xs={3} md={4}>
+                &nbsp;
             </Grid>
-            <Grid container>
-                <Grid item xs={3} md={4}>
-                    &nbsp;
-                </Grid>
-                <Grid className={classes.mainPageLinkContainer} item xs={6} md={4} onClick={props.handleReturnHome}>
-                    <Paragraph color={TypographyColor.LinkBlue}>
-                        Return to main page
-                    </Paragraph>
-                </Grid>
+            <Grid className={classes.mainPageLinkContainer} item xs={6} md={4} onClick={props.handleReturnHome}>
+                <Paragraph color={TypographyColor.LinkBlue}>
+                    Return to main page
+                </Paragraph>
             </Grid>
-        </div>
-    )
+        </Grid>
+    );
 }
 
 export default HomePage;
