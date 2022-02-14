8745fa492fdc38de03f6af3cb52a26155b282b9f
Andrew Deaver <deaver.andrew@gmail.com>
2022-02-14 09:58:39 -0500

    Force refresh of link processing (#431)

--- backend/src/babblegraph/util/bufferedfetch/bufferedfetch.go
+++ backend/src/babblegraph/util/bufferedfetch/bufferedfetch.go
@@ -1,6 +1,7 @@
 package bufferedfetch
 
 import (
+	"babblegraph/util/ctx"
 	"fmt"
 	"log"
 	"reflect"
@@ -52,4 +53,26 @@ func WithNextBufferedValue(key string, fn func(interface{}) error) error {
 	nextVal := fs.i[0]
 	fs.i = append([]interface{}{}, fs.i[1:]...)
 	return fn(nextVal)
+}
+
+func ForceRefill(c ctx.LogContext, key string) error {
+	fs, exists := bufferedState[key]
+	if !exists {
+		return fmt.Errorf("Cannot get next value for bufferedfetch with key %s: does not exist", key)
+	}
+	c.Infof("Bufferedfetch refilling key %s on force", key)
+	refill, err := fs.refillFn()
+	if err != nil {
+		return fmt.Errorf("Error refilling bufferedfetch with key %s: %s", key, err.Error())
+	}
+	switch reflect.TypeOf(refill).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(refill)
+		for i := 0; i < s.Len(); i++ {
+			fs.i = append(fs.i, s.Index(i).Interface())
+		}
+	default:
+		return fmt.Errorf("Error refilling bufferedfetch with key %s: refill func did not return a list", key)
+	}
+	return nil
 }
