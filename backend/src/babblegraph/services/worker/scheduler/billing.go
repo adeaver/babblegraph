903cfadf6704c7e976a3d01506cd4a570a721fda
Andrew Deaver <deaver.andrew@gmail.com>
2022-02-17 12:25:07 -0500

    Add billing sync

---
+++ backend/src/babblegraph/services/worker/scheduler/billing.go
@@ -0,0 +1,98 @@
+package scheduler
+
+import (
+	"babblegraph/model/billing"
+	"babblegraph/model/useraccounts"
+	"babblegraph/model/useraccountsnotifications"
+	"babblegraph/model/users"
+	"babblegraph/util/async"
+	"babblegraph/util/database"
+	"fmt"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+)
+
+func handleSyncBilling(c async.Context) {
+	var userIDsWithExpiredSubscription []users.UserID
+	var premiumNewsletterSyncRequests map[billing.PremiumNewsletterSubscriptionID]billing.PremiumNewsletterSubscriptionUpdateType
+	if err := database.WithTx(func(tx *sqlx.Tx) error {
+		var err error
+		userIDsWithExpiredSubscription, err = useraccounts.GetUserIDsForExpiredSubscriptionQuery(tx)
+		if err != nil {
+			return err
+		}
+		premiumNewsletterSyncRequests, err = billing.GetPremiumNewsletterSyncRequests(tx)
+		return err
+	}); err != nil {
+		c.Errorf("Error getting billing items to sync: %s", err.Error())
+		return
+	}
+	userIDsWithExpirationToSkip := make(map[users.UserID]bool)
+	for premiumSubscriptionID, updateType := range premiumNewsletterSyncRequests {
+		var userID *users.UserID
+		if err := database.WithTx(func(tx *sqlx.Tx) error {
+			premiumNewsletterSubscription, err := billing.GetPremiumNewsletterSubscriptionByID(c, tx, premiumSubscriptionID)
+			if err != nil {
+				return err
+			}
+			switch updateType {
+			case billing.PremiumNewsletterSubscriptionUpdateTypeTransitionToActive:
+				switch premiumNewsletterSubscription.PaymentState {
+				case billing.PaymentStateCreatedUnpaid:
+					c.Infof("Subscription %s is still unpaid, skipping.", premiumSubscriptionID)
+					return billing.MarkPremiumNewsletterSyncRequestForRetry(tx, premiumSubscriptionID)
+				case billing.PaymentStateTrialNoPaymentMethod,
+					billing.PaymentStateTrialPaymentMethodAdded,
+					billing.PaymentStateActive:
+					c.Infof("Subscription %s is now active, updating", premiumSubscriptionID)
+					userID, err = premiumNewsletterSubscription.GetUserID()
+					if err != nil {
+						return err
+					}
+					if err := useraccounts.AddSubscriptionLevelForUser(tx, useraccounts.AddSubscriptionLevelForUserInput{
+						UserID:            *userID,
+						SubscriptionLevel: useraccounts.SubscriptionLevelPremium,
+						ShouldStartActive: true,
+						ExpirationTime:    premiumNewsletterSubscription.CurrentPeriodEnd,
+					}); err != nil {
+						return err
+					}
+					return billing.MarkPremiumNewsletterSyncRequestDone(tx, premiumSubscriptionID)
+				case billing.PaymentStateErrored,
+					billing.PaymentStateTerminated:
+					c.Infof("Subscription %s is terminated, marking done", premiumSubscriptionID)
+					return billing.MarkPremiumNewsletterSyncRequestDone(tx, premiumSubscriptionID)
+				default:
+					return fmt.Errorf("Unrecognized payment state for subscription ID %s: %d", premiumSubscriptionID, premiumNewsletterSubscription.PaymentState)
+				}
+			default:
+				return fmt.Errorf("Unrecognized update type: %s", updateType)
+			}
+		}); err != nil {
+			c.Errorf("Error syncing stripe subscription with ID %s: %s", premiumSubscriptionID, err.Error())
+			continue
+		}
+		if userID != nil {
+			userIDsWithExpirationToSkip[*userID] = true
+		}
+	}
+	for _, userID := range userIDsWithExpiredSubscription {
+		if _, ok := userIDsWithExpirationToSkip[userID]; ok {
+			c.Infof("User ID %s had an update, skipping", userID)
+			continue
+		}
+		if err := database.WithTx(func(tx *sqlx.Tx) error {
+			if err := useraccounts.ExpireSubscriptionForUser(tx, userID); err != nil {
+				return err
+			}
+			if _, err := useraccountsnotifications.EnqueueNotificationRequest(tx, userID, useraccountsnotifications.NotificationTypePremiumSubscriptionCanceled, time.Now().Add(5*time.Minute)); err != nil {
+				return err
+			}
+			// TODO: ensure stripe subscription is canceled
+			return nil
+		}); err != nil {
+			c.Errorf("Error canceling subscription for User %s: %s", userID, err.Error())
+		}
+	}
+}
--- backend/src/babblegraph/services/worker/scheduler/scheduler.go
+++ backend/src/babblegraph/services/worker/scheduler/scheduler.go
@@ -1,7 +1,6 @@
 package scheduler
 
 import (
-	"babblegraph/externalapis/bgstripe"
 	"babblegraph/services/worker/linkprocessing"
 	"babblegraph/util/async"
 	"babblegraph/util/env"
@@ -23,12 +22,11 @@ func StartScheduler(linkProcessor *linkprocessing.LinkProcessor, errs chan error
 		c.AddFunc("30 3 * * *", async.WithContext(errs, "admin-2fa-cleanup", handleCleanUpAdminTwoFactorCodesAndAccessTokens).Func())
 		c.AddFunc("30 4 * * *", async.WithContext(errs, "cleanup-newsletters", handleCleanupOldNewsletter).Func())
 		c.AddFunc("30 12 * * *", async.WithContext(errs, "user-feedback", sendUserFeedbackEmails).Func())
-		c.AddFunc("11 */3 * * *", async.WithContext(errs, "expire-accounts", expireUserAccounts).Func())
 		c.AddFunc("*/1 * * * *", async.WithContext(errs, "pending-verifications", handlePendingVerifications).Func())
 		c.AddFunc("*/3 * * * *", async.WithContext(errs, "forgot-passwords", handlePendingForgotPasswordAttempts).Func())
 		c.AddFunc("*/5 * * * *", async.WithContext(errs, "account-notifications", handlePendingUserAccountNotificationRequests).Func())
-		c.AddFunc("14 */1 * * *", async.WithLogContext(errs, "sync-stripe", bgstripe.ForceSyncStripeEvents).Func())
 		c.AddFunc("*/1 * * * *", async.WithContext(errs, "send-2fa-codes", handleSendAdminTwoFactorAuthenticationCode).Func())
+		c.AddFunc("*/10 * * * *", async.WithContext(errs, "sync-billing", handleSyncBilling).Func())
 	case "local-test-emails",
 		"local":
 		c.AddFunc("*/1 * * * *", async.WithContext(errs, "cleanup-newsletters", handleCleanupOldNewsletter).Func())
@@ -36,11 +34,10 @@ func StartScheduler(linkProcessor *linkprocessing.LinkProcessor, errs chan error
 		c.AddFunc("*/1 * * * *", async.WithContext(errs, "account-notifications", handlePendingUserAccountNotificationRequests).Func())
 		c.AddFunc("*/1 * * * *", async.WithContext(errs, "pending-verifications", handlePendingVerifications).Func())
 		c.AddFunc("*/1 * * * *", async.WithContext(errs, "forgot-passwords", handlePendingForgotPasswordAttempts).Func())
-		c.AddFunc("*/3 * * * *", async.WithContext(errs, "expire-accounts", expireUserAccounts).Func())
 		c.AddFunc("*/5 * * * *", async.WithContext(errs, "archive-forgot-passwords", handleArchiveForgotPasswordAttempts).Func())
 		c.AddFunc("*/30 * * * *", async.WithContext(errs, "refetch", makeRefetchSeedDomainJob(linkProcessor)).Func())
-		c.AddFunc("*/1 * * * *", async.WithLogContext(errs, "sync-stripe", bgstripe.ForceSyncStripeEvents).Func())
 		c.AddFunc("*/1 * * * *", async.WithContext(errs, "send-2fa-codes", handleSendAdminTwoFactorAuthenticationCode).Func())
+		c.AddFunc("*/1 * * * *", async.WithContext(errs, "sync-billing", handleSyncBilling).Func())
 		async.WithContext(errs, "user-feedback", sendUserFeedbackEmails).Func()()
 	case "local-no-email":
 		async.WithContext(errs, "refetch", makeRefetchSeedDomainJob(linkProcessor)).Func()()
