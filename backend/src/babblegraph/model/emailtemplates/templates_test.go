49e49c689a90c018c6f0378302296e52f40baeb9
Andrew Deaver <deaver.andrew@gmail.com>
2022-03-13 16:33:07 -0400

    Last test

--- backend/src/babblegraph/model/emailtemplates/templates_test.go
+++ backend/src/babblegraph/model/emailtemplates/templates_test.go
@@ -94,4 +94,71 @@ func TestCreateNewsletterWithLinksTemplate(t *testing.T) {
 	if !strings.Contains(*html, "babblegraph.com/reinforce") {
 		t.Errorf("Expected reinforcement link")
 	}
+	if !strings.Contains(*html, "Test Link") {
+		t.Errorf("Expected title")
+	}
+	if !strings.Contains(*html, "Test Description") {
+		t.Errorf("Expected description")
+	}
+	if !strings.Contains(*html, "Test Episode") {
+		t.Errorf("Expected episode title")
+	}
+	if !strings.Contains(*html, "This episode is a test") {
+		t.Errorf("Expected episode description")
+	}
+}
+
+func TestCreateNewsletterNoPodcastLinksTemplate(t *testing.T) {
+	userAccessor := &testUserAccessor{
+		userHasAccount: false,
+		userID:         users.UserID("12345"),
+	}
+	testNewsletter := newsletter.NewsletterBody{
+		SetTopicsLink:     ptr.String("babblegraph.com/topics"),
+		ReinforcementLink: "babblegraph.com/reinforce",
+		PreferencesLink:   ptr.String("babblegraph.com/preferences"),
+		Categories: []newsletter.Category{
+			{
+				Name: ptr.String("Test Category"),
+				Links: []newsletter.Link{
+					{
+						DocumentID:       documents.DocumentID("test"),
+						ImageURL:         ptr.String("babblegraph.com"),
+						Title:            ptr.String("Test Link"),
+						Description:      ptr.String("Test Description"),
+						URL:              "babblegraph.com",
+						PaywallReportURL: "babblegraph.com",
+						Domain: &newsletter.Domain{
+							FlagAsset: "babblegraph.com",
+							Name:      "Babblegraph",
+						},
+					},
+				},
+				PodcastLinks: []newsletter.PodcastLink{},
+			},
+		},
+	}
+	html, err := MakeNewsletterHTML(MakeNewsletterHTMLInput{
+		EmailRecordID: email.NewEmailRecordID(),
+		UserAccessor:  userAccessor,
+		Body:          testNewsletter,
+	})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if len(*html) == 0 {
+		t.Fatalf("Got empty body")
+	}
+	if !strings.Contains(*html, "babblegraph.com/topics") {
+		t.Errorf("Expected topics link")
+	}
+	if !strings.Contains(*html, "babblegraph.com/reinforce") {
+		t.Errorf("Expected reinforcement link")
+	}
+	if !strings.Contains(*html, "Test Link") {
+		t.Errorf("Expected title")
+	}
+	if !strings.Contains(*html, "Test Description") {
+		t.Errorf("Expected description")
+	}
 }
