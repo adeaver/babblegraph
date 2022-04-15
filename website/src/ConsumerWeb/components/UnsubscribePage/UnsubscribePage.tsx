1d264a7b210ee1a293096a5d6cb25a5c2321f989
Andrew Deaver <deaver.andrew@gmail.com>
2022-04-15 10:35:46 -0400

    Add Unsubscribe Modal for common issues (#452)

--- website/src/ConsumerWeb/components/UnsubscribePage/UnsubscribePage.tsx
+++ website/src/ConsumerWeb/components/UnsubscribePage/UnsubscribePage.tsx
@@ -4,17 +4,24 @@ import { RouteComponentProps } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Snackbar from '@material-ui/core/Snackbar';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Modal from '@material-ui/core/Modal';
+import ClearIcon from '@material-ui/icons/Clear';
 
 import Alert from 'common/components/Alert/Alert';
+import Color from 'common/styles/colors';
 import CenteredComponent from 'common/components/CenteredComponent/CenteredComponent';
 import DisplayCard from 'common/components/DisplayCard/DisplayCard';
 import LoadingSpinner from 'common/components/LoadingSpinner/LoadingSpinner';
 import DisplayCardHeader from 'common/components/DisplayCard/DisplayCardHeader';
 import Paragraph, { Size } from 'common/typography/Paragraph';
-import { PrimaryButton } from 'common/components/Button/Button';
+import { PrimaryButton, WarningButton } from 'common/components/Button/Button';
+import { PrimaryCheckbox } from 'common/components/Checkbox/Checkbox';
+import { Heading2, Heading4 } from 'common/typography/Heading';
 import { PrimaryTextField } from 'common/components/TextField/TextField';
 import { Alignment, TypographyColor } from 'common/typography/common';
 import Form from 'common/components/Form/Form';
+import { setLocation } from 'util/window/Location';
 
 import {
     UnsubscribeError,
@@ -45,7 +52,27 @@ const styleClasses = makeStyles({
     formContainer: {
         padding: '10px 0',
     },
+    submitButton: {
+        width: '100%',
+    },
+    closeModalIconContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+    },
+    closeModalIcon: {
+        color: Color.BorderGray,
+        cursor: 'pointer',
+    },
 });
+
+enum CommonUnsubscribeReason {
+    TooManyEmails = 'Too many emails',
+    TooManyStories = 'Too many stories per email',
+    BadTime = 'Email arrives at a bad time',
+    NotInteresting = 'Articles aren’t interesting',
+    UnusualArticles = 'Unusual or offensive articles',
+};
 
 const errorMessages = {
     [UnsubscribeError.MissingEmail]: "You need to include your email address to unsubscribe.",
@@ -79,6 +106,18 @@ const UnsubscribePage = withUserProfileInformation<UnsubscribePageOwnProps>(
             setEmailAddress((event.target as HTMLInputElement).value);
         };
 
+        const [ checkedCommonUnsubscribeReasons, setCheckedCommonUnsubscribeReasons ] = useState<CommonUnsubscribeReason[]>([]);
+        const handleToggleCommonUnsubscribeReason = (r: CommonUnsubscribeReason) => {
+            setCheckedCommonUnsubscribeReasons(
+                checkedCommonUnsubscribeReasons.indexOf(r) === -1 ? (
+                    checkedCommonUnsubscribeReasons.concat(r)
+                ) : (
+                    checkedCommonUnsubscribeReasons.filter((checkedReason: CommonUnsubscribeReason) => checkedReason !== r)
+                ));
+        }
+
+        const [ isModalOpen, setIsModalOpen ] = useState<boolean>(false);
+
         const [ unsubscribeReason, setUnsubscribeReason ] = useState<string | null>(null);
         const [ validationError, setValidationError ] = useState<string | null>(null);
         const handleUnsubscribeReasonChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -94,10 +133,28 @@ const UnsubscribePage = withUserProfileInformation<UnsubscribePageOwnProps>(
         const [ didUpdate, setDidUpdate ] = useState<boolean>(false);
         const [ errorMessage, setErrorMessage ] = useState<string>(null);
         const handleSubmit = () => {
+            const shouldShowModal = checkedCommonUnsubscribeReasons.some(
+                (r: CommonUnsubscribeReason) => {
+                    return (
+                        r === CommonUnsubscribeReason.TooManyEmails ||
+                        r === CommonUnsubscribeReason.TooManyStories ||
+                        r === CommonUnsubscribeReason.BadTime
+                    );
+            });
+            if (shouldShowModal && !isModalOpen) {
+                setIsModalOpen(true);
+                return;
+            }
+            const finalUnsubscribeReason = checkedCommonUnsubscribeReasons
+                .map((r: CommonUnsubscribeReason) => `${r}`)
+                .concat(unsubscribeReason)
+                .filter((s: string | null) => !!s)
+                .join(";");
+            setIsModalOpen(false);
             setIsLoading(true);
             unsubscribeUser({
                 token: token,
-                unsubscribeReason: unsubscribeReason,
+                unsubscribeReason: finalUnsubscribeReason,
                 emailAddress: props.userProfile.hasAccount ? undefined : emailAddress,
             },
             (resp: UnsubscribeResponse) => {
@@ -138,14 +195,33 @@ const UnsubscribePage = withUserProfileInformation<UnsubscribePageOwnProps>(
                                         )
                                     }
                                     </Paragraph>
+                                    <Heading4 align={Alignment.Left} color={TypographyColor.Primary}>
+                                        Help improve Babblegraph (optional)
+                                    </Heading4>
+                                    <Paragraph align={Alignment.Left}>
+                                        Tell us what we could be doing better.
+                                    </Paragraph>
                                     <Grid container>
+                                        {
+                                            Object.values(CommonUnsubscribeReason).map((r: CommonUnsubscribeReason) => (
+                                                <Grid item key={r} xs={12} md={6}>
+                                                    <FormControlLabel
+                                                        control={
+                                                            <PrimaryCheckbox
+                                                                checked={checkedCommonUnsubscribeReasons.indexOf(r) !== -1}
+                                                                onChange={() => { handleToggleCommonUnsubscribeReason(r) }}
+                                                                name={`checkbox-${r}`} />
+                                                        }
+                                                        label={r} />
+                                                </Grid>
+                                            ))
+                                        }
                                         <Grid item xs={12}>
                                             <PrimaryTextField
                                                 id="unsubscribe-reason"
                                                 className={classes.textField}
-                                                label="Reason for unsubscribing (optional)"
+                                                label="Other reason"
                                                 variant="outlined"
-                                                rows={3}
                                                 error={validationError}
                                                 onChange={handleUnsubscribeReasonChange}
                                                 multiline />
@@ -186,6 +262,15 @@ const UnsubscribePage = withUserProfileInformation<UnsubscribePageOwnProps>(
                                             </div>
                                         )
                                     }
+                                    {
+                                        isModalOpen && (
+                                            <UnsubscribeModal
+                                                subscriptionManagementToken={props.match.params.token}
+                                                checkedCommonUnsubscribeReasons={checkedCommonUnsubscribeReasons}
+                                                handleConfirmUnsubscribe={handleSubmit}
+                                                handleCloseModal={() => {setIsModalOpen(false)}} />
+                                        )
+                                    }
                             </Form>
                         )
                     }
@@ -200,5 +285,64 @@ const UnsubscribePage = withUserProfileInformation<UnsubscribePageOwnProps>(
         );
     }
 );
+
+type UnsubscribeModalProps = {
+    subscriptionManagementToken: string;
+    checkedCommonUnsubscribeReasons: Array<CommonUnsubscribeReason>;
+
+    handleConfirmUnsubscribe: () => void;
+    handleCloseModal: () => void;
+}
+
+const UnsubscribeModal = (props: UnsubscribeModalProps) => {
+    const handleGoToSettings = () => {
+        setLocation(`/manage/${props.subscriptionManagementToken}/preferences`);
+    }
+
+    const classes = styleClasses();
+    return (
+        <Modal
+            open={true}
+            onClose={props.handleCloseModal}>
+            <CenteredComponent>
+                <DisplayCard>
+                    <Grid container>
+                        <Grid item xs={10}>
+                            <Heading2 color={TypographyColor.Primary} align={Alignment.Left}>
+                                We have a fix for some of your problems!
+                            </Heading2>
+                        </Grid>
+                        <Grid item className={classes.closeModalIconContainer} xs={2}>
+                            <ClearIcon className={classes.closeModalIcon} onClick={props.handleCloseModal} />
+                        </Grid>
+                    </Grid>
+                    <Paragraph align={Alignment.Left}>
+                        We got a lot of feedback that readers of Babblegraph were feeling overwhelmed by the amount of content.
+                    </Paragraph>
+                    <Paragraph align={Alignment.Left}>
+                        Babblegraph now allows users to select which days they receive emails, what time they receive them, and how many articles they receive with each email.
+                    </Paragraph>
+                    <Grid container>
+                        <Grid item className={classes.submitButtonContainer} xs={12} md={6}>
+                            <PrimaryButton
+                                className={classes.submitButton}
+                                onClick={handleGoToSettings}>
+                                Take me to settings
+                            </PrimaryButton>
+                        </Grid>
+                        <Grid item className={classes.submitButtonContainer} xs={12} md={6}>
+                            <WarningButton
+                                className={classes.submitButton}
+                                onClick={props.handleConfirmUnsubscribe}
+                                type='submit'>
+                                Continue to Unsubscribe
+                            </WarningButton>
+                        </Grid>
+                    </Grid>
+                </DisplayCard>
+            </CenteredComponent>
+        </Modal>
+    );
+}
 
 export default UnsubscribePage;
